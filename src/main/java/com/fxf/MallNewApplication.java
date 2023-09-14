c5b7d37c609851fb4630f5542a33ec7680673279
fxf <1632248095@qq.com>
2023-09-14 14:39:57 +0800

    初始化工程

---
+++ src/main/java/com/fxf/MallNewApplication.java
@@ -0,0 +1,13 @@
+package com.fxf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallNewApplication.class, args);
+	}
+
+}
